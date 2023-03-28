81e763f505e319200427bcd78e2e7ee3d283ce7e
satendra mani gautam <smgautam15@gmail.com>
2023-03-28 14:16:27 +0530

    folder structure

---
+++ instaclone/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import AppRouter from './routers/AppRoutes';
+
+function App() {
+  return (
+    <div className="App-contener">
+      <AppRouter />
+      
+    </div>
+  );
+}
+
+export default App;
+
